36d011aeeccc2779f1c2a382904cfb01320b54d5
kkyu8925 <kkyu8925@gmail.com>
2022-04-11 22:38:26 +0900

    nestjs

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,4 +1,3 @@
-import { CatsService } from './cats/cats.service';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
